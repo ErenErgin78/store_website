43f0120be22814a234364913e6f262fa200d50b1
Eren Ergin <115938507+ErenErgin78@users.noreply.github.com>
2024-09-21 20:20:48 +0300

    Modelleri Entities isimli bir başka yere aldım

---
+++ Store/Entities/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models;
+
+public class Product
+{
+    [Key]
+    public int Product_Id { get; set; }
+    public String? Product_Name { get; set; } = String.Empty;
+    public decimal Product_Price { get; set; }
+}
