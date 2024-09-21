823357443fb578dc261b912f8d74f34ad51600fb
Eren Ergin <115938507+ErenErgin78@users.noreply.github.com>
2024-09-21 23:53:47 +0300

    RepositoryBase ProductBase

---
+++ Store/Repositories/RepositoryBase.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Repositories.Contracts;
+
+namespace Repositories
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T>
+    where T : class, new()   //Tipin new'lenebilir ve referans tipli olması için
+    {
+        protected readonly RepositoryContext _context;
+
+        protected RepositoryBase(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> FindAll(bool trackChanges)
+        {
+            return trackChanges ? _context.Set<T>() : _context.Set<T>().AsNoTracking();
+        }
+    }
+}
