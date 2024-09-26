e2e8cf4935569068229e84df23447c4a8b5d5564
Eren Ergin <115938507+ErenErgin78@users.noreply.github.com>
2024-09-26 22:27:01 +0300

    Admin paneli için layout ve navbar

--- Store/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ Store/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f7b8ce28e1d8c81ddceeedb53799144150b346f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad421d02298fadda1c3d1cd76faa4cb4a7ec1646")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
