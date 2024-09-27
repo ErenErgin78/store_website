07142edfdf1de140297c5e976f52761f3aea2e65
Eren Ergin <115938507+ErenErgin78@users.noreply.github.com>
2024-09-27 22:32:28 +0300

    Admin product için cshtml sayfası ve controller kısmına Service referansı

--- Store/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Store/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad421d02298fadda1c3d1cd76faa4cb4a7ec1646")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2e8cf4935569068229e84df23447c4a8b5d5564")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
