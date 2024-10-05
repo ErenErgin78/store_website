de2e9a06d38af83b731b6ebbadbc4a6d7f28aafc
Eren Ergin <115938507+ErenErgin78@users.noreply.github.com>
2024-10-05 23:14:37 +0300

    Contact sayfası

--- Store/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Store/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+face108b58df719280b7c96cca7c36d98a29e196")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1cb95b056bb95f994a33f35196436a440c6d9ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Store/Store_Web/Program.cs
+++ Store/Store_Web/Program.cs
@@ -7,6 +7,7 @@ using Services.Contracts;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.AddDbContext<RepositoryContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"),
@@ -42,6 +43,8 @@ app.UseEndpoints(endpoints =>
     endpoints.MapControllerRoute(
         name: "default",
         pattern: "{controller=Home}/{action=Index}/{id?}");
+        
+    endpoints.MapRazorPages();
 });
 #pragma warning restore ASP0014 // Suggest using top level route registrations
 
