37c02120bfc8339abee0bb13cab366e3db8ee31b
Eren Ergin <115938507+ErenErgin78@users.noreply.github.com>
2024-10-09 19:12:43 +0300

    Details yanına sepete ekle simgesi

--- Store/Store_Web/Pages/Cart.cshtml.cs
+++ Store/Store_Web/Pages/Cart.cshtml.cs
@@ -1,16 +1,41 @@
 using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Contracts;
 
 namespace Store_Web.Pages
 {
     public class CartModel : PageModel
     {
+        private readonly IServiceManager  _manager;
+
+        public CartModel(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
         public Cart Cart { get; set; }
         public String ReturnUrl { get; set; } = "/";
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-            
+        }
+
+        public IActionResult OnPost(int product_Id, string returnUrl)
+        {
+            Product? product = _manager.ProductService.GetProductById(product_Id, false);
+
+            if(product is not null)
+            {
+                Cart.AddItem(product,1);
+            } 
+            return Page();
+        }
+
+        public IActionResult OnPostRemove(int id, string returnUrl)
+        {
+            Cart.RemoveLine(Cart.Lines.First(i => i.Product.Product_Id.Equals(id)).Product);
+            return Page();
         }
 
     }
